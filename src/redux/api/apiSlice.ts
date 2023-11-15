d73f82d27ca58e8bf5e9bb7b7cd1555d33a4e745
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-11-15 21:18:45 +0600

    Ready to deploy

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "userApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/v1/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://book-collection-backend.vercel.app/api/v1/",
+  }),
   tagTypes: ["Books", "Review", "Status", "User"],
   endpoints: () => ({}),
 });
