748102da28bbd7b2a3fc72022f2e4667eb1b58cb
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2023-11-19 00:08:12 +0600

    Error Handled

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://book-collection-backend.vercel.app/api/v1/",
+    // baseUrl: "https://book-collection-backend.vercel.app/api/v1/",
+    baseUrl: "http://localhost:5000/api/v1/",
   }),
   tagTypes: ["Books", "Review", "Status", "User"],
   endpoints: () => ({}),
